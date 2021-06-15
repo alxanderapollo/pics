bb85db0e628af56c5348e1cd555a9b9fe883b321
Alex Salazar <alxanderapollo@gmail.com>
2021-06-14 22:49:22 -0400

    got image height and wrote a call back function using the event listener to return the proper value to the console

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 class  ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.imageRef = React.createRef();
+    }
+    //life cylce method
+    componentDidMount(){
+      // call back function to return the hight of an image
+      this.imageRef.current.addEventListener('load',this.setSpans); 
+    }
+
+    //getting the span value
+    setSpans = () =>{
+        console.log(this.imageRef.current.clientHeight)
+
+    }
+
     render() {
         //destructing description and urls
         const {description,urls} = this.props.image;
         return (<div>
             {/* getting props and displaying the description  */}
-            <img alt={description}
+            <img ref= {this.imageRef} alt={description}
             // reciving image and displaying it it
             src={urls.regular}
             
