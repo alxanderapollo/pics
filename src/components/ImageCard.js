6a0ae25175af4ab5b3ce05867520b0d406504c32
Alex Salazar <alxanderapollo@gmail.com>
2021-06-17 22:41:03 -0400

    finally done with all implementations

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,24 +3,33 @@ import React from 'react';
 class  ImageCard extends React.Component {
     constructor(props) {
         super(props);
-        this.imageRef = React.createRef();
+        //CreateRef allows us to reach into the dom and interact with an element
+        this.imageRef = React.createRef(); //always initlized in the constructor, and to wire it we pass it as a prop to the desired element 
+        this.state = {spans:0} //spans initlized and set to 0
     }
     //life cylce method
     componentDidMount(){
       // call back function to return the hight of an image
+      //
       this.imageRef.current.addEventListener('load',this.setSpans); 
     }
 
     //getting the span value
     setSpans = () =>{
-        console.log(this.imageRef.current.clientHeight)
+      const height =  this.imageRef.current.clientHeight
+      //returing the necessary span hights required for the image to fully display on the screen and the hights is being divided
+      //by the number of pixels 
+      const spans = Math.ceil(height /10);//rounded up so that itll go to the next highest row 
 
+      this.setState({spans});
     }
 
     render() {
         //destructing description and urls
         const {description,urls} = this.props.image;
-        return (<div>
+        return (
+            // is spaning 
+        <div style={{gridRowEnd:`span ${this.state.spans}`}}>
             {/* getting props and displaying the description  */}
             <img ref= {this.imageRef} alt={description}
             // reciving image and displaying it it
